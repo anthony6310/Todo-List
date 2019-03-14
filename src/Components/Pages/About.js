a41c114af4efd1c47992cfa9f414e5b9a36679e9
anthony6310 <42251943+anthony6310@users.noreply.github.com>
2019-03-13 21:23:52 -0400

    initial commit

---
+++ src/Components/Pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a todo list app v1.0.0. it is part of a React Crash
+          course.
+      </p>
+    </div>
+  )
+}
